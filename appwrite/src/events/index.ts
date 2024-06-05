f7d04bbde496c3f33ce334d0a3833937b653507e
vynxc <vynxcs@gmail.com>
2024-06-05 10:53:17 -0400

    test db event databases . *

--- appwrite/src/events/index.ts
+++ appwrite/src/events/index.ts
@@ -2,7 +2,7 @@ import { AppwriteEventHandler } from "@vynxc/appwrite-utils";
 
 const eventHandler = new AppwriteEventHandler();
 
-eventHandler.on("account.create", async (event, { log, res }) => {
+eventHandler.on("databases . *", async (event, { log, res }) => {
 	log(event);
 	return res.json({ message: "Account created" });
 });
