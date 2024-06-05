4628bf2793aea21c1b49107519f3b63ef18fbe43
vynxc <vynxcs@gmail.com>
2024-06-05 11:12:09 -0400

    header viewer

--- appwrite/src/elysia/index.ts
+++ appwrite/src/elysia/index.ts
@@ -1,7 +1,14 @@
 import Elysia from "elysia";
 import { swagger } from "@elysiajs/swagger";
 const app = new Elysia()
-	.use(swagger({ path: "/" }))
+	//.use(swagger({ path: "/" }))
+	.all("/", (data) => {
+		const toReturn: Record<string, string>[] = [];
+		data.request.headers.forEach((value, key) => {
+			toReturn.push({ [key]: value });
+		});
+		return toReturn;
+	})
 	.get("/friends/add/:username", ({ params }) => {
 		const username = params.username;
 		return `Hello ${username}`;
