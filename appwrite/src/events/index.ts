96ecf7ab5613bb60ebf7844b13c14954caf249af
vynxc <vynxcs@gmail.com>
2024-06-05 10:59:22 -0400

    fix werid spacing on event

--- appwrite/src/events/index.ts
+++ appwrite/src/events/index.ts
@@ -2,7 +2,7 @@ import { AppwriteEventHandler } from "@vynxc/appwrite-utils";
 
 const eventHandler = new AppwriteEventHandler();
 
-eventHandler.on("databases . *", async (event, { log, res }) => {
+eventHandler.on("databases.*", async (event, { log, res }) => {
 	log(event);
 	return res.json({ message: "Account created" });
 });
