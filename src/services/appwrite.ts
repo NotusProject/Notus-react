1809824d09b3a1b141987e4eae435893c01e8a7c
vynxc <vynxcs@gmail.com>
2024-06-06 08:48:08 -0400

    update to use createExecution

--- src/services/appwrite.ts
+++ src/services/appwrite.ts
@@ -1,16 +1,55 @@
-import { Client, Account } from "appwrite";
+import { Client, Account, Functions, ExecutionMethod } from "appwrite";
 import { treaty } from "@elysiajs/eden";
 import { App } from "../../appwrite/src/elysia";
 
 const client = new Client()
-	.setEndpoint("https://cloud.appwrite.io/v1")
-	.setProject("665cdeb50027cdbdd34f");
+	.setEndpoint("https://appwrite.wasimhub.dev/v1")
+	.setProject("notus");
 
 export const api = treaty<App>("https://6660df2bc1f22b396b2a.wasimhub.dev", {
 	async fetcher(input, init) {
-		return call(input, init);
+		return callClient(input, init);
 	},
 });
+
+const functions = new Functions(client);
+async function callClient(input: RequestInfo | URL, init?: RequestInit) {
+	let url: URL;
+	if (typeof input === "string") {
+		url = new URL(input);
+	} else {
+		url = input as URL;
+	}
+
+	const path = url.pathname + url.search + url.hash;
+	const body = init?.body;
+	const method = init?.method as ExecutionMethod;
+	let bodyString: string | undefined = undefined;
+	if (body) {
+		bodyString = await convertBodyToString(body);
+	}
+	const headers = init?.headers;
+
+	const execution = await functions.createExecution(
+		"6660df2a00211037c8c4", // functionId
+		bodyString, // body (optional)
+		false, // async (optional)
+		path, // path (optional)
+		method, // method (optional)
+		headers // headers (optional)
+	);
+	const resHeaders: Record<string, string> = {};
+	execution.responseHeaders.forEach((header) => {
+		resHeaders[header.name] = header.value;
+	});
+	const response = new Response(execution.responseBody, {
+		headers: resHeaders,
+		status: execution.responseStatusCode,
+		statusText: execution.responseStatusCode.toString(),
+	});
+	return response;
+}
+//@ts-ignore
 async function call(input: RequestInfo | URL, init?: RequestInit) {
 	init = init ?? {};
 	init.headers = init.headers ?? {};
@@ -23,6 +62,9 @@ async function call(input: RequestInfo | URL, init?: RequestInit) {
 			headers["X-Fallback-Cookies"] = cookieFallback;
 		}
 	}
+	init.headers = headers;
+	console.log(init.headers);
+
 	const response = await fetch(input, init);
 	if (response.headers.get("X-Fallback-Cookies")) {
 		window.localStorage.setItem(
@@ -35,3 +77,18 @@ async function call(input: RequestInfo | URL, init?: RequestInit) {
 const account = new Account(client);
 
 export { client, account };
+
+async function convertBodyToString(body: BodyInit): Promise<string> {
+	if (typeof body === "string") {
+		return body;
+	} else if (body instanceof Blob) {
+		return await body.text();
+	} else if (body instanceof FormData) {
+		return new URLSearchParams(body as any).toString();
+	} else if (body instanceof URLSearchParams) {
+		return body.toString();
+	} else {
+		// Handle other types of BodyInit if needed
+		throw new Error("Unsupported body type");
+	}
+}
