1f1c54e51ef98df3e277677c53d9d44c3622485d
vynxc <vynxcs@gmail.com>
2024-06-06 19:23:15 -0400

    update to handle errors ::BAD

--- appwrite/src/index.ts
+++ appwrite/src/index.ts
@@ -7,13 +7,25 @@ import {
 
 import app from "./elysia";
 import eventHandler from "./events";
+import { logger } from "./appwrite";
 
 export default async function server(ctx: Context): Promise<Response> {
-	const eventrsp = await eventHandler.pipe(ctx);
-	if (eventrsp) {
-		return eventrsp;
+	logger.log = ctx.log;
+	logger.error = ctx.log;
+	try {
+		const eventrsp = await eventHandler.pipe(ctx);
+		if (eventrsp) {
+			return eventrsp;
+		}
+		const request = toRequest(ctx.req);
+		const honoResposne = await app.handle(request);
+		return await toResponse(honoResposne);
+	} catch (e) {
+		if (e instanceof Error) {
+			logger.error(e.message);
+		}
+		logger.error("Unhandled error");
+
+		return await toResponse(Response.error());
 	}
-	const request = toRequest(ctx.req);
-	const honoResposne = await app.handle(request);
-	return await toResponse(honoResposne);
 }
