5cb7a437b20834f2936b200e493a19a7639aa342
vynxc <vynxcs@gmail.com>
2024-06-06 07:30:06 -0400

    fix merger

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,22 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./utils/ProtectedRoute.tsx";
 import FriendsPage from "./pages/Friends.tsx";
-import { api } from "./services/appwrite.ts";
 
 function Home() {
-	const [response, setResponse] = React.useState("No response yet");
-	async function handleClick() {
-		const treaty = await api.friends.add({ username: "wasim" }).get();
-		const data = await treaty.response.text();
-		setResponse(data);
-	}
-
 	return (
 		<div className={"h-[999px]"}>
-			<div className={"h-96"}>
-				<p>{response}</p>
-				<button onClick={handleClick}>Click me</button>
-			</div>
+			<div className={"h-96"}>z</div>
 			<div className={"h-96"}>dd</div>
 			<div className={"h-96"}>dd</div>
 			<div className={"h-96"}>dd</div>
