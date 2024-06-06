36897f83556afa39434d7d9af10ded813182f3c0
vynxc <vynxcs@gmail.com>
2024-06-06 19:40:52 -0400

    update

--- appwrite/src/index.ts
+++ appwrite/src/index.ts
@@ -11,7 +11,7 @@ import { logger } from "./appwrite";
 
 export default async function server(ctx: Context): Promise<Response> {
 	logger.log = ctx.log;
-	logger.error = ctx.log;
+	logger.error = ctx.error;
 	try {
 		const eventrsp = await eventHandler.pipe(ctx);
 		if (eventrsp) {
@@ -26,6 +26,6 @@ export default async function server(ctx: Context): Promise<Response> {
 		}
 		logger.error("Unhandled error");
 
-		return await toResponse(Response.error());
+		return ctx.res.json({ message: "Unhandled error" }, 500);
 	}
 }
